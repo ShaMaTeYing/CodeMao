7cf77ee196a2d2688c07a8207c6f2a9182718191
wuying <wuying@onecode.com.cn>
2018-06-21 14:55:50 +0800

    add admin

---
+++ Home/Lib/Action/TrainAdminAction.class.php
@@ -0,0 +1,19 @@
+<?php
+// 本类由系统自动生成，仅供测试用途
+class TrainAdminAction extends BaseAction {
+	/* 构造函数 */
+	function _initialize(){
+		$userinfo=session('userinfo');
+		if(!$userinfo||$userinfo['root']<1){
+			$this->error("非法访问，请先登录!",U('User/showLogin'));
+		}
+		$login=M('user')->where(array('id'=>$userinfo['id']))->find();
+		$loginStatus=session('loginStatus');
+	   //控制切换登录窗口
+		$this->assign('loginStatus',session('loginStatus')?session('loginStatus'):0);
+		if(session('loginStatus'))//登录成功则传值给模板变量
+		{
+			$this->assign('userinfoData',session('userinfo'));
+		}
+	}
+}
