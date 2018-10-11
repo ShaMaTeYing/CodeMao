99914612a2c994dec74e4b805b18dcd0a931ab96
wuying <wuying@onecode.com.cn>
2018-10-11 19:31:52 +0800

    add photo

--- Home/Lib/Action/BaseAction.class.php
+++ Home/Lib/Action/BaseAction.class.php
@@ -91,11 +91,11 @@ class BaseAction extends Action {
 //		$token='23ljwoejuoiasjfasd';
 		if($token)
 		{
-				$resData=$this->getPostData($token);
-				$resData = json_decode($resData,true);
+//				$resData=$this->getPostData($token);
+//				$resData = json_decode($resData,true);
 //				$resData=$this->getTestPostData();
-//			$resData=$this->post_json_data($token);
-//			$resData = json_decode($resData,true);
+			$resData=$this->post_json_data($token);
+			$resData = json_decode($resData,true);
 //			dump($resData);
 //			die;
 			if(intval($resData['errorcode'])==0&&$resData['data']['userId'])
